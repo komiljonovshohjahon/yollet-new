85c4100a6497b6c7b2aa80a0609d9385b564693c
Shokhjakhon Komiljonov <komiljonovshohjahon@gmail.com>
2021-04-19 14:15:40 +0900

    Contact form ready

--- js/app.js
+++ js/app.js
@@ -1,49 +1,22 @@
-const name = document.querySelector("#name");
-const surname = document.querySelector("#message");
-const email = document.querySelector("#email");
-const form = document.querySelector(".form-container");
+// JQUERY METHOD
 
-// form.addEventListener("submit", function (e) {
-//   e.preventDefault();
-//   let formData = {
-//     name: name.value,
-//     surname: surname.value,
-//     email: email.value,
-//   };
+$(".form-container").submit(function (event, done) {
+  var form = $(this);
+  // server side validation
+  // every contact form field is required
+  event.preventDefault();
+  $.ajax({
+    type: "POST",
+    url: "http://localhost:5000/post",
+    data: form.serialize(),
+  });
+  $("#submitted").removeClass("hidden").addClass("open");
 
-//   let xhr = new XMLHttpRequest();
-//   xhr.open("POST", "http://localhost:5000/post");
-//   xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-//   xhr.onload = function () {
-//     console.log(xhr.responseText);
-//     if (xhr.responseText == "success") {
-//       alert("Email sent");
-//       name.value = "";
-//       surname.value = "";
-//       email.value = "";
-//     } else {
-//       alert("Error");
-//     }
-//   };
-//   //   xhr.send(formData);
-//   xhr.send("ASASAS");
-// });
-
-form.addEventListener("submit", function (e) {
-  //   e.preventDefault();
-  let formData = {
-    name: name.value,
-    surname: surname.value,
-    email: email.value,
-  };
-
-  fetch("http://localhost:5000/post", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-    body: {
-      formData,
-    },
-  }).then((result) => {});
+  // .done(function (data) {
+  //   if (data.status == "OK") {
+  //     $("#contact-success").modal("show");
+  //   } else {
+  //     return done("Please fill all of the fields.", false);
+  //   }
+  // });
 });
